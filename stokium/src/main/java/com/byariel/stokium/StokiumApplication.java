3a956cba4d9be72e9e795e04e0b55c2f05a7ab88
byariel <aromero@alumnos.unsada.edu.ar>
2026-01-21 14:02:13 -0300

    feat: initialize Spring Boot project

---
+++ stokium/src/main/java/com/byariel/stokium/StokiumApplication.java
@@ -0,0 +1,13 @@
+package com.byariel.stokium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StokiumApplication.class, args);
+    }
+
+}
---
+++ stokium/src/test/java/com/byariel/stokium/StokiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byariel.stokium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
